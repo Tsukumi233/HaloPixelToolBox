81dc6a31ad115fd04622469de15ba985ca254c3d
XFEstudio <mail@xfegzs.com>
2026-07-01 05:16:01 +0800

    优化部分方法名称

--- HaloPixelToolBox/HaloPixelToolBox/Utilities/Helpers/UpgradeHelper.cs
+++ HaloPixelToolBox/HaloPixelToolBox/Utilities/Helpers/UpgradeHelper.cs
@@ -7,7 +7,7 @@ namespace HaloPixelToolBox.Utilities.Helpers;
 
 public static class UpgradeHelper
 {
-    public static string RequestAddress => "http://upgrade.api.xfegzs.com/upgrade";
+    public static string RequestAddress => "http://upgrade.api.xfe.studio/upgrade";
     public static Upgrader Upgrader { get; set; } = new(RequestAddress);
     public static Version Version => Assembly.GetExecutingAssembly().GetName().Version ?? new Version(1, 0, 0);
 
@@ -15,7 +15,7 @@ public static class UpgradeHelper
     /// 检测是否需要更新
     /// </summary>
     /// <returns></returns>
-    public static async Task<bool> NeedUpgrade()
+    public static async Task<bool> CheckUpgrade()
     {
         try
         {
